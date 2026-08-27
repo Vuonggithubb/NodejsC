30596a1aaba434cbd9071a681363ed338d61690b
Vuonggithubb <fanvuong99@gmail.com>
2026-08-27 21:22:48 +0700

    a

--- src/server.js
+++ src/server.js
@@ -1,19 +1,21 @@
 const express = require("express");
-const app = express();
-const port = 8000;
 const path = require("path");
+
+const app = express();
+const port = process.env.PORT || 8000;
+const hostname = process.env.HOST_NAME || "localhost";
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hellommmkjkjk World!");
 });
 
 app.get("/abc", (req, res) => {
   res.render("sample.ejs");
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
